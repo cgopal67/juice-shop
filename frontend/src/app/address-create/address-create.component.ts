e99b39c1363565c7ba49e959590f757b18e94c1c
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-07-16 16:51:45 -0400

    update

--- frontend/src/app/address-create/address-create.component.ts
+++ frontend/src/app/address-create/address-create.component.ts
@@ -1,4 +1,35 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/georrychen/ChXBlockDemo/releases/tag/v13.3.0
+*    Source File: address-create.component.ts
+*    
+*    Copyrights:
+*      copyright (c) 2014-2022 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    
+*    Project: https://owasp-juice.shop
+*    Release: https://github.com/juice-shop/juice-shop/releases/tag/v12.3.0
+*    Source File: address-create.component.ts
+*    
+*    Copyrights:
+*      copyright (c) 2014-2021 bjoern kimminich
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
