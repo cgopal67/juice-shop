b6f00ab121beec7f40b40fefff51cb4191902401
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-07-16 16:51:43 -0400

    update

--- routes/profileImageFileUpload.ts
+++ routes/profileImageFileUpload.ts
@@ -1,4 +1,23 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/georrychen/ChXBlockDemo/releases/tag/v13.3.0
+*    Source File: profileImageFileUpload.ts
+*    
+*    Copyrights:
+*      copyright (c) 2014-2022 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
