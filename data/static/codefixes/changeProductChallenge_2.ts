90f3926c83cba3e803dec9fab06085c079e9779e
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-07-16 16:52:02 -0400

    update

--- data/static/codefixes/changeProductChallenge_2.ts
+++ data/static/codefixes/changeProductChallenge_2.ts
@@ -1,3 +1,24 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/georrychen/ChXBlockDemo/releases/tag/v13.3.0
+*    Source File: changeProductChallenge_2.ts
+*    
+*    Copyrights:
+*      copyright © by bjoern kimminich & the owasp juice shop contributors 2014-2021
+*      copyright (c) 2014-2021 bjoern kimminich
+*      copyright (c) 2014-2021 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /** Authorization **/
   /* Baskets: Unauthorized users are not allowed to access baskets */
   app.use('/rest/basket', security.isAuthorized(), security.appendUserId())
