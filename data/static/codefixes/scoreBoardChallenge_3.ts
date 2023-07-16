71756c4de63b1abcb1a4d930374e0e169a258ee9
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-07-16 16:51:43 -0400

    update

--- data/static/codefixes/scoreBoardChallenge_3.ts
+++ data/static/codefixes/scoreBoardChallenge_3.ts
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/georrychen/ChXBlockDemo/releases/tag/v13.3.0
+*    Source File: scoreBoardChallenge_3.ts
+*    
+*    Copyrights:
+*      copyright © by bjoern kimminich & the owasp juice shop contributors
+*      copyright (c) 2014-2022 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 const routes: Routes = [
   {
     path: 'administration',
