d99a4b4506ba27cc5672cf71d9fda3ae364b15fe
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-07-16 16:50:59 -0400

    update

--- data/static/codefixes/accessLogDisclosureChallenge_4.ts
+++ data/static/codefixes/accessLogDisclosureChallenge_4.ts
@@ -1,3 +1,24 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/georrychen/ChXBlockDemo/releases/tag/v13.3.0
+*    Source File: accessLogDisclosureChallenge_4.ts
+*    
+*    Copyrights:
+*      copyright © by bjoern kimminich & the owasp juice shop contributors 2014-2021
+*      copyright (c) 2014-2021 bjoern kimminich
+*      copyright (c) 2014-2021 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /* /ftp directory browsing and file download */
   app.use('/ftp', serveIndexMiddleware, serveIndex('ftp', { icons: true }))
   app.use('/ftp(?!/quarantine)/:file', fileServer())
