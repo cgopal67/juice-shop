fbd46a06797c970c5b97f8d1d13b7dd2154255f5
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-07-16 16:51:52 -0400

    update

--- data/static/codefixes/xssBonusChallenge_3.ts
+++ data/static/codefixes/xssBonusChallenge_3.ts
@@ -1,3 +1,24 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/georrychen/ChXBlockDemo/releases/tag/v13.3.0
+*    Source File: xssBonusChallenge_3.ts
+*    
+*    Copyrights:
+*      copyright © by bjoern kimminich & the owasp juice shop contributors 2014-2021
+*      copyright (c) 2014-2021 bjoern kimminich
+*      copyright (c) 2014-2021 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 filterTable () {
     let queryParam: string = this.route.snapshot.queryParams.q
     if (queryParam) {
