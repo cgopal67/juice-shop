f0e6e5f37179736b0d57b7db17fc701bd29490a2
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-07-16 16:51:34 -0400

    update

--- frontend/src/assets/private/CopyShader.js
+++ frontend/src/assets/private/CopyShader.js
@@ -1,3 +1,27 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://threejs.org/
+*    Release: https://github.com/mrdoob/three.js/releases/tag/r71
+*    Source File: CopyShader.js
+*    
+*    Copyrights:
+*      copyright � 2004 by magenta ltd. all rights reserved
+*      copyright 2011 data arts team, google creative lab
+*      copyright 2011 google inc. all rights reserved
+*      copyright (c) 2009-2010 marcus geelnard
+*      copyright (c) 2010-2012 three.js authors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * @author alteredq / http://alteredqualia.com/
  *
