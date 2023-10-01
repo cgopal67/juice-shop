23acd7d673a3d190a5436682b1ad016fa890c100
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-10-01 12:19:57 -0400

    update

--- lib/challengeUtils.ts
+++ lib/challengeUtils.ts
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://owasp-juice.shop
+*    Release: https://github.com/juice-shop/juice-shop/releases/tag/v13.2.2
+*    Source File: utils.ts
+*    
+*    Copyrights:
+*      copyright (c) 2014-2022 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 import { Op } from 'sequelize'
 import { ChallengeModel } from '../models/challenge'
 import logger from './logger'
