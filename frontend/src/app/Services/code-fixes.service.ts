d90e86c36be54c70b960ba9392a96c839772d2da
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-10-02 12:43:31 -0400

    update

--- frontend/src/app/Services/code-fixes.service.ts
+++ frontend/src/app/Services/code-fixes.service.ts
@@ -1,3 +1,24 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://owasp-juice.shop
+*    Release: https://github.com/juice-shop/juice-shop/releases/tag/v13.3.0
+*    Source File: code-fixes.service.ts
+*    
+*    Copyrights:
+*      copyright © by bjoern kimminich 2014-2021
+*      copyright (c) 2014-2021 bjoern kimminich
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 import { Injectable } from '@angular/core'
 import { environment } from '../../environments/environment'
 import { HttpClient } from '@angular/common/http'
