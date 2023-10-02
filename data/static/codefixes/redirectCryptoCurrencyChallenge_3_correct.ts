1b1676a9e5fd016c4ddc24b9b712517096b7503f
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-10-02 12:43:19 -0400

    update

--- data/static/codefixes/redirectCryptoCurrencyChallenge_3_correct.ts
+++ data/static/codefixes/redirectCryptoCurrencyChallenge_3_correct.ts
@@ -1,3 +1,24 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://owasp-juice.shop
+*    Release: https://github.com/juice-shop/juice-shop/releases/tag/v13.3.0
+*    Source File: redirectCryptoCurrencyChallenge_3_correct.ts
+*    
+*    Copyrights:
+*      copyright © by bjoern kimminich & the owasp juice shop contributors
+*      copyright (c) 2014-2021 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 export const redirectAllowlist = new Set([
   'https://github.com/bkimminich/juice-shop',
   'http://shop.spreadshirt.com/juiceshop',
