5f513ae2eb33cefac9f0351de6da058d38b2eb6b
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-10-02 12:44:13 -0400

    update

--- frontend/src/app/last-login-ip/last-login-ip.component.ts
+++ frontend/src/app/last-login-ip/last-login-ip.component.ts
@@ -1,4 +1,24 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://owasp-juice.shop
+*    Release: https://github.com/juice-shop/juice-shop/releases/tag/v13.3.0
+*    Source File: last-login-ip.component.ts
+*    
+*    Copyrights:
+*      copyright (c) 2014-2022 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright (c) 2014-2023 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
