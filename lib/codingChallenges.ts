0704db710b8c8423b947d09a885dea73fdddf272
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-10-02 12:44:36 -0400

    update

--- lib/codingChallenges.ts
+++ lib/codingChallenges.ts
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://owasp-juice.shop
+*    Release: https://github.com/juice-shop/juice-shop/releases/tag/v13.1.0
+*    Source File: vulnCodeSnippet.ts
+*    
+*    Copyrights:
+*      copyright (c) 2014-2022 bjoern kimminich & the owasp juice shop contributors
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 import fs from 'fs/promises'
 import path from 'path'
 import logger from './logger'
