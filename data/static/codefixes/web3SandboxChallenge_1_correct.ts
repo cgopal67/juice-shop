255b039773338f4644cf00827d1a9b8b7509cd89
cgopal67 <137306000+cgopal67@users.noreply.github.com>
2023-10-02 12:42:55 -0400

    update

--- data/static/codefixes/web3SandboxChallenge_1_correct.ts
+++ data/static/codefixes/web3SandboxChallenge_1_correct.ts
@@ -1,3 +1,23 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://owasp-juice.shop
+*    Release: https://github.com/juice-shop/juice-shop/releases/tag/v12.6.1
+*    Source File: app.routing.ts
+*    
+*    Copyrights:
+*      copyright (c) 2014-2020 bjoern kimminich
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 const routes: Routes = [
   {
     path: 'administration',
